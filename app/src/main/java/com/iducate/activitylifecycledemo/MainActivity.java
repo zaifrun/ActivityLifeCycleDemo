4dc9a473f6a3a13e717baa40a1820886e482e9f9
Martin Knudsen <martinknud@gmail.com>
2015-12-08 11:51:04 +0100

    Initial commit

---
+++ app/src/main/java/com/iducate/activitylifecycledemo/MainActivity.java
@@ -0,0 +1,56 @@
+package com.iducate.activitylifecycledemo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+	
+	private final String TAG = "ActivityLifeCycle";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Log.d(TAG, "onCreate() called");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart() called");
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.d(TAG, "onRestart() called");
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
+
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop() called");
+	}
+	
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
+	}
+
+}
